96e18db56155f06c2de5a2a006e04f73d853c7d2
doaibu13rhn <alkaffraihan13@gmail.com>
2023-09-22 10:51:40 +0700

    pre push

--- index.js
+++ index.js
@@ -83,6 +83,213 @@ server.delete("/users/:id", async (req, res) => {
     }
 });
 
+server.get("/users", async (req, res) => {
+    try {
+        const sql = `select  Username, address FROM users`;
+        const result = await db.query(sql);
+        res.status(201).json({
+            msg: "success",
+            result: result.rows
+        })
+    } catch (error) {
+        res.status(500).json({
+            msg: "error cuy",
+            error: error,
+        })
+    }
+});
+
+server.post("/users", (req, res) => {
+    const { body } = req;
+    const sql = "INSERT INTO users (Username, email, password, address, created_at) VALUES ($1,$2,$3,$4,$5)";
+    const values = [body.Username, body.email, body.password, body.address, body.created_at];
+    db.query(sql, values)
+        .then((data) => {
+            res.status(201).json({
+                msg: "successfully added new users data",
+                result: data.rows,
+            });
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json({
+                msg: "internal server error",
+            });
+        });
+});
+
+server.patch("/users/:id", async (req, res) => {
+    try {
+        const { body, params } = req;
+        const sql = `update users set address = $1, updated_at = now() where usersid = $2`;
+        const values = [body.address, params.id];
+        await db.query(sql, values);
+        res.status(200).json({
+            msg: `update address for user id ${params.id} has changed to ${body.address}`,
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            msg: "internal server error",
+        })
+    }
+});
+
+server.delete("/users/:id", async (req, res) => {
+    try {
+        const { params } = req;
+        const sql = `delete from users where usersid = $1 returning Username`;
+        const values = [params.id];
+        const data = await db.query(sql, values);
+        res.status(200).json({
+            msg: `user id ${params.id} has been deleted`,
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            msg: "internal server error",
+        })
+    }
+});
+
+server.get("/users", async (req, res) => {
+    try {
+        const sql = `select  Username, address FROM users`;
+        const result = await db.query(sql);
+        res.status(201).json({
+            msg: "success",
+            result: result.rows
+        })
+    } catch (error) {
+        res.status(500).json({
+            msg: "error cuy",
+            error: error,
+        })
+    }
+});
+
+server.post("/users", (req, res) => {
+    const { body } = req;
+    const sql = "INSERT INTO users (Username, email, password, address, created_at) VALUES ($1,$2,$3,$4,$5)";
+    const values = [body.Username, body.email, body.password, body.address, body.created_at];
+    db.query(sql, values)
+        .then((data) => {
+            res.status(201).json({
+                msg: "successfully added new users data",
+                result: data.rows,
+            });
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json({
+                msg: "internal server error",
+            });
+        });
+});
+
+server.patch("/users/:id", async (req, res) => {
+    try {
+        const { body, params } = req;
+        const sql = `update users set address = $1, updated_at = now() where usersid = $2`;
+        const values = [body.address, params.id];
+        await db.query(sql, values);
+        res.status(200).json({
+            msg: `update address for user id ${params.id} has changed to ${body.address}`,
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            msg: "internal server error",
+        })
+    }
+});
+
+server.delete("/users/:id", async (req, res) => {
+    try {
+        const { params } = req;
+        const sql = `delete from users where usersid = $1 returning Username`;
+        const values = [params.id];
+        const data = await db.query(sql, values);
+        res.status(200).json({
+            msg: `user id ${params.id} has been deleted`,
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            msg: "internal server error",
+        })
+    }
+});
+
+server.get("/users", async (req, res) => {
+    try {
+        const sql = `select  Username, address FROM users`;
+        const result = await db.query(sql);
+        res.status(201).json({
+            msg: "success",
+            result: result.rows
+        })
+    } catch (error) {
+        res.status(500).json({
+            msg: "error cuy",
+            error: error,
+        })
+    }
+});
+
+server.post("/users", (req, res) => {
+    const { body } = req;
+    const sql = "INSERT INTO users (Username, email, password, address, created_at) VALUES ($1,$2,$3,$4,$5)";
+    const values = [body.Username, body.email, body.password, body.address, body.created_at];
+    db.query(sql, values)
+        .then((data) => {
+            res.status(201).json({
+                msg: "successfully added new users data",
+                result: data.rows,
+            });
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json({
+                msg: "internal server error",
+            });
+        });
+});
+
+server.patch("/users/:id", async (req, res) => {
+    try {
+        const { body, params } = req;
+        const sql = `update users set address = $1, updated_at = now() where usersid = $2`;
+        const values = [body.address, params.id];
+        await db.query(sql, values);
+        res.status(200).json({
+            msg: `update address for user id ${params.id} has changed to ${body.address}`,
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            msg: "internal server error",
+        })
+    }
+});
+
+server.delete("/users/:id", async (req, res) => {
+    try {
+        const { params } = req;
+        const sql = `delete from users where usersid = $1 returning Username`;
+        const values = [params.id];
+        const data = await db.query(sql, values);
+        res.status(200).json({
+            msg: `user id ${params.id} has been deleted`,
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            msg: "internal server error",
+        })
+    }
+});
+
 server.listen(8000, () => {
     console.log("server is running at port 8000")
 })
